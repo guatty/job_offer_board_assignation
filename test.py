a79265b01262532d250f0b5eac082ad809efde25
Julien Eyzat <julien.eyzat@telecomnancy.eu>
2018-11-13 09:57:55 +0100

    Truc

--- test.py
+++ test.py
@@ -5,11 +5,10 @@ import pandas as pd
 # print(tf.__version__)
 # print(keras.__version__)
 
-df = pd.read_csv('../df_stats.csv')
+df = pd.read_csv('data/df_stats.csv')
 
 # print(list(df))
 
-del df['Unnamed: 0']
 del df['title.1']
 del df['name.1']
 del df['job_group_id']
@@ -18,10 +17,18 @@ del df['enabled']
 del df['limit_cv']
 del df['uppdate']
 del df['budgetleft']
+del df['employer']
+
+# df['']
+
+df = df.dropna()
+
+
+# print(df.corr())
 
 # print(df['job_board_id'])
 
-df.to_csv('./preprocessed_campaigns.csv')
+df.to_csv('data/preprocessed_campaigns.csv')
 
 # a = df['job_board_id'].corr(df['title'], method='spearman') # 0.015116500377894835
 
--- test2.py
+++ test2.py
@@ -1,15 +1,54 @@
 import pandas as pd
 
-df = pd.read_csv('./preprocessed_campaigns.csv')
+df = pd.read_csv('data/preprocessed_campaigns.csv')
 
-print(list(df))
+# print(list(df))
 
 i=0
-new_df = pd.DataFrame(columns=['id', 'title', 'category', 'country', 'cpc', 'name', 'keywords', 'description', 'job_type', 'employer', 'job', 'job_board_id', 'amount_action_0', 'amount_action_1', 'amount_action_2', 'amount_action_3', 'amount_action_4', 'budgetmax', 'budgetleft', 'creation'])
+columns_name = ['id', 'title', 'category', 'country', 'cpc', 'name', 'keywords', 'description', 'job_type', 'job', 'job_board_id', 'amount_action_0', 'amount_action_1', 'amount_action_2', 'amount_action_3', 'amount_action_4', 'budgetmax', 'creation']
+new_df = pd.DataFrame(columns=columns_name)
 for row in df.groupby(['id', 'creation']):
-    print(row[1])
-    print(row[1]['id'])
-    print(type(row[1]))
-    # df.loc[i] = [row[1]['id'], row[1]['id'], row[1]['id'], row[1]['id']]
+    if i % 1000 == 0:
+        print(i)
+    # print(row[1])
+    # print(row[1]['id'].iloc[0])
+    # print(type(row[1]['id']))
+    line = [0] * len(columns_name)
+    j = 0
+    for column in columns_name:
+        if column == "amount_action_0":
+            if not (row[1][row[1]['action'] == 0].empty):
+                line[j] = row[1][row[1]['action'] == 0 ]['amount_action'].iloc[0]
+            else:
+                line[j] = 0
+        elif column == "amount_action_1":
+            if not (row[1][row[1]['action'] == 1].empty):
+                line[j] = row[1][row[1]['action'] == 1 ]['amount_action'].iloc[0]
+            else:
+                line[j] = 0
+        elif column == "amount_action_2":
+            if not (row[1][row[1]['action'] == 2].empty):
+                line[j] = row[1][row[1]['action'] == 2 ]['amount_action'].iloc[0]
+            else:
+                line[j] = 0
+        elif column == "amount_action_3":
+            if not (row[1][row[1]['action'] == 3].empty):
+                line[j] = row[1][row[1]['action'] == 3 ]['amount_action'].iloc[0]
+            else:
+                line[j] = 0
+        elif column == "amount_action_4":
+            if not (row[1][row[1]['action'] == 4].empty):
+                line[j] = row[1][row[1]['action'] == 4 ]['amount_action'].iloc[0]
+            else:
+                line[j] = 0
+            # print(row[1][row[1]['action'] == 1 ]['amount_action'].iloc[0])
+            # print(row[1][row[1]['action'] == 1 ]['action'].iloc[0] == 1)
+            # YYY = df[ df['action'] == 1 ]['amount_action']
+        else:
+            # print(line.append(row[1]['id'].iloc[0]))
+            line[j] = row[1][column].iloc[0]
+        j+=1
+    new_df.loc[i] = line
     i+=1
-    break
+
+new_df.to_csv('data/cleaned_preprocessed_campaigns.csv')
