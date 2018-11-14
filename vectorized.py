45d5ec17dc5bf41b7b4b1ac5bcf5c55a5acff2ce
marvelousJee <geoffreysaldo@gmail.com>
2018-11-14 21:41:34 +0100

    nvx vectorized

--- vectorized.py
+++ vectorized.py
@@ -8,12 +8,11 @@ Created on Mon Nov 12 18:13:26 2018
 
 import pandas as pd # manipulation de csv
 import frequency_terms as freq
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer
 
 
-# tf_idf
+# tf_idf 
 import nltk
-# nltk.download('stopwords')
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -23,6 +22,7 @@ from nltk.corpus import stopwords  # text preproccess
 import html2text
 
 import csv
+
 
 ENGLISH_STOPWORDS = set(stopwords.words("english"))
 FRENCH_STOPWORDS = set(stopwords.words("french"))
@@ -36,11 +36,10 @@ FRENCH_STOPWORDS.add('leurs')
 FRENCH_STOPWORDS.add('desormais')
 FRENCH_STOPWORDS.add('nous')
 FRENCH_STOPWORDS.add('si')
+FRENCH_STOPWORDS.add('les')
+FRENCH_STOPWORDS.add('lui')
 
-MAX_NB_WORDS= 200000
-
-NB_KEYWORDS = 10
-
+MAX_NB_WORDS= 1000
 #print(html2text.html2text(contenu_col))
 #df = pd.read_csv('test_correl.csv', delimiter=',', encoding="utf-8-sig")
 
@@ -56,7 +55,7 @@ def preprocess(text):
     text = filter(lambda word: word not in ENGLISH_STOPWORDS, text)
     text = filter(lambda word: word not in FRENCH_STOPWORDS, text)
     return " ".join(text)
-
+    
 
 def clean_tab(tab):
     for i in range(len(tab)):
@@ -70,12 +69,13 @@ def remove_html_pattern(column):
         element_without_html = html2text.html2text(element)
         without_html_tab.append(element_without_html)
     return without_html_tab
-
+        
 def vectorize_column(data_column):
     tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
     tokenizer.fit_on_texts(data_column)
+    word_index = tokenizer.word_index
     vector_data = tokenizer.texts_to_sequences(data_column)
-    return vector_data
+    return vector_data,word_index
 
 def replace_text_by_vector(dataframe,column_to_remove,column_to_add,name_column_to_add):
     del dataframe[column_to_remove]
@@ -83,8 +83,8 @@ def replace_text_by_vector(dataframe,column_to_remove,column_to_add,name_column_
     return dataframe
 
 def convert_df_to_csv(dataframe):
-    dataframe.to_csv('vectorized_campaigns.csv')
-
+    dataframe.to_csv('vectorized_campaigns.csv') 
+    
 #=================================================================
 # tf_idf functions
 #=================================================================
@@ -100,7 +100,7 @@ def compute_tf_idf(text,wd,cv,feature_names):
     #generate tf-idf for the given document
     tf_idf_vector=tfidf_transformer.transform(cv.transform([text]))
     sorted_items=freq.sort_coo(tf_idf_vector.tocoo())
-    keywords=freq.extract_topn_from_vector(feature_names,sorted_items,NB_KEYWORDS)
+    keywords=freq.extract_topn_from_vector(feature_names,sorted_items,15)
     return keywords
 
 def compute_tf_idf_column(column,wd,cv,feature_names):
@@ -116,68 +116,50 @@ def get_keys(array_of_tf_idf):
         keys = dictionnaire.keys()
         tab_keys.append(keys)
     return tab_keys
-
-
-
-
+    
+    
+    
+        
 #=================================================================
 # test
 #=================================================================
 
 
 
-df = pd.read_csv('data/cleaned_preprocessed_campaigns.csv', delimiter=',', encoding="utf-8")
+df = pd.read_csv('small_data.csv', delimiter=',', encoding="utf-8")
 
 # Vectorization of description column
 
 description_column = df["description"]
 clean_description_column = remove_html_pattern(description_column)
 clean_description_column = clean_tab(clean_description_column)
-
-cv, word_count_vec, feature_names = prepare_tf_idf(clean_description_column)
-keywords = compute_tf_idf_column(clean_description_column,word_count_vec,cv,feature_names)
-keys = get_keys(keywords)
-
-print("a")
-
-all_ids = []
-all_words = []
-for words in keys:
-    for key in words:
-        if not (key in all_words):
-            all_words.append(key)
-
-print(len(all_words))
+vector_description,word_index = vectorize_column(clean_description_column)
 
 with open('data/truc2.csv', 'w') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
     my_header = []
-    my_header.append("my_id")
-    my_header.append("my_title")
-    for i in range(10):
-        my_header.append("keyword_"+str(i))
-    # for word in all_words:
-    #     my_header.append(word)
+    for mot in word_index.keys():
+        my_header.append(mot)
     spamwriter.writerow(my_header)
-    for index in range(len(keywords)):
-        if not (df['id'][index] in all_ids):
-            final_list = []
-            all_ids.append(df['id'][index])
-            final_list.append(df['id'][index])
-            final_list.append(df['title'][index].replace('"', ''))
-            # for word in all_words:
-            #     found = False
-            #     for key in keys[index]:
-            #         if (key == word):
-            #             final_list.append(1)
-            #             found = True
-            #         if not found:
-            #             final_list.append("?")
-            # spamwriter.writerow(final_list)
-            for key in keys[index]:
-                    final_list.append(key)
-            if (len(keys[index]) == NB_KEYWORDS):
-                spamwriter.writerow(final_list)
+    for description in vector_description:
+        liste_binaire=[]
+        for i in range(len(word_index)) :
+            if i in description:
+                liste_binaire.append(1)
+            else:
+                liste_binaire.append(0)
+        spamwriter.writerow(liste_binaire)
+            
+        
+        
+'''
+cv, word_count_vec, feature_names = prepare_tf_idf(clean_description_column)
+keywords = compute_tf_idf_column(clean_description_column,word_count_vec,cv,feature_names)
+
+
+keys = get_keys(keywords)
+'''
+
 
 '''
 vector_description = vectorize_column(clean_description_column)
@@ -219,3 +201,18 @@ df = replace_text_by_vector(df,"job_type",vector_job_type,"vector_job_type")
 
 convert_df_to_csv(df)
 '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
